98974efb48c1f40124acfb721efe300c270c99be
Ambient.Impact <i@ambientimpact.com>
2019-03-12 10:01:16 -0400

    Moved Grunt tasks to individual files and upgraded various Node packages.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,66 +1,23 @@
 module.exports = function(grunt) {
+	'use strict';
 
-	grunt.initConfig({
+	// Load our Grunt task configs from external files in the 'grunt' directory.
+	var configs = require('load-grunt-configs')(grunt, {
+		config: {
+			src: 'grunt/*'
+		}
+	});
+
+	// Initialize the Grunt config with the read config plus the package and
+	// paths merged in.
+	grunt.initConfig(Object.assign(configs, {
 		pkg:				grunt.file.readJSON('package.json'),
 
 		themePaths:			'ambientimpact_base,' +
 							'ambientimpact_site',
 		stylesheetPaths:	'stylesheets,components',
-		javascriptPaths:	'javascript,components',
-
-		librariesPath:		'assets/vendor',
-
-		sass: {
-			module: {
-				options: {
-					outputStyle:	'compressed',
-					sourceMap:		true
-				},
-				files: [{
-					src:
-						'{<%= themePaths %>}/{<%= stylesheetPaths %>}/**/' +
-						'*.scss',
-					ext:	'.css',
-					extDot:	'last',
-					expand:	true,
-				}]
-			}
-		},
-		autoprefixer: {
-			module: {
-				options: {
-					map: true
-				},
-				files: [{
-					src:
-						'{<%= themePaths %>}/{<%= stylesheetPaths %>}/**/' +
-						'*.css',
-					ext:	'.css',
-					extDot:	'last',
-					expand:	true,
-				}]
-			}
-		},
-		uglify: {
-			module: {
-				options: {
-					compress: {
-						// Note that this removes all console.* calls, not just
-						// console.log():
-						// https://github.com/gruntjs/grunt-contrib-uglify#turn-off-console-warnings
-						drop_console: true
-					}
-				},
-				src:	[
-					'{<%= themePaths %>}/{<%= javascriptPaths %>}/**/*.js',
-					'!**/*.min.js'
-				],
-				ext:	'.min.js',
-				extDot:	'last',
-				expand:	true,
-			}
-		}
-	});
+		javascriptPaths:	'javascript,components'
+	}));
 
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-autoprefixer');
