1c8a63c2a6370370e53202411bdc211b066e1d96
Ambient.Impact <i@ambientimpact.com>
2019-03-16 14:22:04 -0400

    Furore font is now loaded.

---
+++ ambientimpact_site/javascript/fonts.js
@@ -0,0 +1,14 @@
+/* -----------------------------------------------------------------------------
+  Ambient.Impact - Theme - Fonts
+----------------------------------------------------------------------------- */
+
+AmbientImpact.on('font', function(aiFont) {
+AmbientImpact.addComponent('siteThemeFont', function(siteThemeFont, $) {
+  'use strict';
+
+  // Load fonts.
+  aiFont.load({
+    furore: 'Furore'
+  });
+});
+});
