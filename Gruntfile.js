3e7b046c590d8a7da3e92616bf3ae9925b776ef2
Ambient.Impact <i@ambientimpact.com>
2019-07-05 19:23:53 -0400

    Converted Gruntfile.js to 2 space indent.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,36 @@
 module.exports = function(grunt) {
-	'use strict';
+  'use strict';
 
-	// Load our Grunt task configs from external files in the 'grunt' directory.
-	var configs = require('load-grunt-configs')(grunt, {
-		config: {
-			src: 'grunt/*'
-		}
-	});
+  // Load our Grunt task configs from external files in the 'grunt' directory.
+  var configs = require('load-grunt-configs')(grunt, {
+    config: {
+      src: 'grunt/*'
+    }
+  });
 
-	// Initialize the Grunt config with the read config plus the package and
-	// paths merged in.
-	grunt.initConfig(Object.assign(configs, {
-		pkg:				grunt.file.readJSON('package.json'),
+  // Initialize the Grunt config with the read config plus the package and paths
+  // merged in.
+  grunt.initConfig(Object.assign(configs, {
+    pkg:              grunt.file.readJSON('package.json'),
 
-		themePaths:			'ambientimpact_base,' +
-							'ambientimpact_site',
-		stylesheetPaths:	'stylesheets,components',
-		javascriptPaths:	'javascript,components'
-	}));
+    themePaths:       [
+      'ambientimpact_base',
+      'ambientimpact_site',
+    ].join(','),
+    stylesheetPaths:  'stylesheets,components',
+    javascriptPaths:  'javascript,components'
+  }));
 
-	grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-postcss');
 
-	grunt.registerTask('all', [
-		'sass',
-		'postcss',
-	]);
+  grunt.registerTask('all', [
+    'sass',
+    'postcss',
+  ]);
 
-	grunt.registerTask('css', [
-		'sass',
-		'postcss',
-	]);
+  grunt.registerTask('css', [
+    'sass',
+    'postcss',
+  ]);
 };
