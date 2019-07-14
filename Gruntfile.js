9552b55decc6f3cc8e37553565594596f15eb674
Ambient.Impact <i@ambientimpact.com>
2019-07-14 08:57:23 -0400

    Switched from load-grunt-configs to load-grunt-config for security + updates.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,27 +2,17 @@ module.exports = function(grunt) {
   'use strict';
 
   // Load our Grunt task configs from external files in the 'grunt' directory.
-  var configs = require('load-grunt-configs')(grunt, {
-    config: {
-      src: 'grunt/*'
+  require('load-grunt-config')(grunt, {
+    init: true,
+    data: {
+      themePaths:       [
+        'ambientimpact_base',
+        'ambientimpact_site',
+      ].join(','),
+      stylesheetPaths:  'stylesheets,components',
+      javascriptPaths:  'javascript,components'
     }
   });
-
-  // Initialize the Grunt config with the read config plus the package and paths
-  // merged in.
-  grunt.initConfig(Object.assign(configs, {
-    pkg:              grunt.file.readJSON('package.json'),
-
-    themePaths:       [
-      'ambientimpact_base',
-      'ambientimpact_site',
-    ].join(','),
-    stylesheetPaths:  'stylesheets,components',
-    javascriptPaths:  'javascript,components'
-  }));
-
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-postcss');
 
   grunt.registerTask('all', [
     'sass',
--- security.md
+++ security.md
@@ -1,5 +1,3 @@
 The following security fixes are found in package.json:
 
-* "diff" is >=3.5.0 to fix [WS-2018-0590 - Regular Expression Denial of Service (ReDoS) attacks](https://github.com/kpdecker/jsdiff/commit/2aec4298639bf30fb88a00b356bf404d3551b8c0)
-* "lodash" is >=4.17.11 to fix [prototype pollution](https://www.npmjs.com/advisories/782)
-* "tar" is >=2.2.2 to fix [CVE-2018-20834](https://nvd.nist.gov/vuln/detail/CVE-2018-20834)
+
