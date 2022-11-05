a92178f3612491fcbc605a08db08921d43eeb0fe
Matei Stanca <i@ambientimpact.com>
2022-11-05 18:15:37 -0400

    Refactored Webpack config to Symfony Webpack Encore.

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,12 @@ const baseThemeImporter = require(
 );
 const componentPaths = require('ambientimpact-drupal-modules/componentPaths');
 const easingGradients = require('postcss-easing-gradients');
+const Encore = require('@symfony/webpack-encore');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const glob = require('glob');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
-const SourceMapDevToolPlugin = require('webpack/lib/SourceMapDevToolPlugin');
-
-const isDev = (process.env.NODE_ENV !== 'production');
 
 const distPath = '.webpack-dist';
 
@@ -56,147 +54,136 @@ function getGlobbedEntries() {
 
 };
 
-/**
- * Array of plug-in instances to pass to Webpack.
- *
- * @type {Array}
- */
-let plugins = [
-  new RemoveEmptyScriptsPlugin(),
-  new MiniCssExtractPlugin(),
-  // @see https://www.npmjs.com/package/favicons-webpack-plugin
+// @see https://symfony.com/doc/current/frontend/encore/installation.html#creating-the-webpack-config-js-file
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+.setOutputPath(path.resolve(__dirname, (outputToSourcePaths ? '.' : distPath)))
+
+// Encore will complain if the public path doesn't start with a slash.
+// Unfortunately, it doesn't seem Webpack's automatic public path works here.
+//
+// @see https://webpack.js.org/guides/public-path/#automatic-publicpath
+.setPublicPath('/')
+.setManifestKeyPrefix('')
+
+// We output multiple files.
+.disableSingleRuntimeChunk()
+
+.configureFilenames({
+
+  // Since Webpack started out primarily for building JavaScript applications,
+  // it always outputs a JS files, even if empty. We place these in a temporary
+  // directory by default. Note that the 'webpack-remove-empty-scripts' plug-in
+  // should prevent these being output, but if there's an error while running
+  // Webpack, you'll get a nice 'temp' directory you can just delete.
+  js: 'temp/[name].js',
+
+  // Assets are left at their original locations and not hashed. The [query]
+  // must be left in to ensure any query string specified in the CSS is
+  // preserved.
   //
-  // @todo Switch to using the generated manifest.webmanifest and
-  //   browserconfig.xml? The paths don't seem easily customizable.
-  new FaviconsWebpackPlugin({
+  // @see https://stackoverflow.com/questions/68737296/disable-asset-bundling-in-webpack-5#68768905
+  //
+  // @see https://github.com/webpack-contrib/css-loader/issues/889#issuecomment-1298907914
+  assets: '[file][query]',
 
-    // @todo Create a maskable logo variant for the logoMaskable config option.
-    //
-    // @see https://maskable.app/
-    logo:         './ambientimpact_site/images/icons/icon.png',
-    outputPath:   './ambientimpact_site/images/icons/generated',
+})
+.addEntries(getGlobbedEntries())
 
-    mode:     'webapp',
-    devMode:  'webapp',
+// Clean out any previously built files in case of source files being removed or
+// renamed.
+.cleanupOutputBeforeBuild(['**/*.css', '**/*.css.map'])
 
-    favicons: {
+.enableSourceMaps(!Encore.isProduction())
 
-      appName:      'Ambient.Impact',
-      appShortName: 'Ambient.Impact',
+// We don't use Babel so we can probably just remove all presets to speed it up.
+//
+// @see https://github.com/symfony/webpack-encore/issues/154#issuecomment-361277968
+.configureBabel(function(config) {
+  config.presets = [];
+})
 
-      start_url: '/',
+// Remove any empty scripts Webpack would generate as we aren't a primarily
+// JavaScript-based app and only output CSS and assets.
+.addPlugin(new RemoveEmptyScriptsPlugin())
 
-      background: '#ffffff',
-      theme_color: '#c07300',
+// @see https://www.npmjs.com/package/favicons-webpack-plugin
+//
+// @todo Switch to using the generated manifest.webmanifest and
+//   browserconfig.xml? The paths don't seem easily customizable.
+.addPlugin(new FaviconsWebpackPlugin({
 
-      display:  'browser',
-      lang:     'en-CA',
-      // @todo This doesn't seem to add a version query string?
-      version:  '1',
+  // @todo Create a maskable logo variant for the logoMaskable config option.
+  //
+  // @see https://maskable.app/
+  logo:       './ambientimpact_site/images/icons/icon.png',
+  outputPath: './ambientimpact_site/images/icons/generated',
 
-      icons: {
+  favicons: {
 
-        // We provide our own rather than have them generated.
-        windows: false,
+    appName:      'Ambient.Impact',
+    appShortName: 'Ambient.Impact',
 
-        yandex: false,
+    start_url: '/',
 
-      },
+    background: '#ffffff',
+    theme_color: '#c07300',
+
+    display:  'browser',
+    lang:     'en-CA',
+
+    // @todo This doesn't seem to add a version query string?
+    version:  '1',
+
+    icons: {
+
+      // We provide our own rather than have them generated.
+      windows: false,
+
+      yandex: false,
 
     },
 
-  }),
-];
-
-if (isDev === true) {
-  plugins.push(
-    new SourceMapDevToolPlugin({
-      filename: '[file].map',
-    })
-  );
-}
-
-module.exports = {
-
-  mode:     isDev ? 'development' : 'production',
-  devtool:  isDev ? 'eval-cheap-module-source-map': false,
-
-  entry: getGlobbedEntries,
-
-  plugins: plugins,
-
-  output: {
-
-    path: path.resolve(__dirname, (outputToSourcePaths ? '.' : distPath)),
-
-    // Be very careful with this - if outputting to the source paths, this must
-    // not be true or it'll delete everything contained in the directory without
-    // warning.
-    clean: !outputToSourcePaths,
-
-    // Since Webpack started out primarily for building JavaScript applications,
-    // it always outputs a JS files, even if empty. We place these in a
-    // temporary directory by default.
-    filename: 'temp/[name].js',
-
-    // Asset bundling/copying disabled for now.
-    //
-    // @see https://stackoverflow.com/questions/68737296/disable-asset-bundling-in-webpack-5#68768905
-    assetModuleFilename: '[file]',
-
   },
 
-  module: {
-    rules: [
-      {
-        test: /\.(scss)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: isDev,
-              importLoaders: 2,
-            },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: isDev,
-              postcssOptions: {
-                plugins: [
-                  easingGradients(),
-                  autoprefixer(),
-                ],
-              },
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isDev,
-              sassOptions: {
-                importer: [
-                  baseThemeImporter,
-                ],
-                includePaths: componentPaths().all,
-              }
-            },
-          },
-        ],
-      },
-     {
-        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
-        type: 'asset/resource',
-        // Asset bundling/copying disabled for now.
-        //
-        // @see https://stackoverflow.com/questions/68737296/disable-asset-bundling-in-webpack-5#68768905
-        generator: {
-          emit: false,
-        },
-      },
+}))
+
+.enableSassLoader(function(options) {
+  options.sassOptions = {
+    importer: [baseThemeImporter],
+    includePaths: componentPaths().all,
+  };
+})
+.enablePostCssLoader(function(options) {
+  options.postcssOptions = {
+    plugins: [
+      easingGradients(),
+      autoprefixer(),
     ],
-  }
-};
+  };
+})
+// Re-enable automatic public path for paths referenced in CSS.
+//
+// @see https://github.com/symfony/webpack-encore/issues/915#issuecomment-1189319882
+.configureMiniCssExtractPlugin(function(config) {
+  config.publicPath = 'auto';
+})
+
+// Disable the Encore image rule because we provide our own loader config.
+.configureImageRule({enabled: false})
+
+// This disables asset bundling/copying for certain asset types.
+//
+// @see https://stackoverflow.com/questions/68737296/disable-asset-bundling-in-webpack-5#68768905
+.addLoader({
+  test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+  type: 'asset/resource',
+  generator: {
+    emit: false,
+  },
+})
+
+module.exports = Encore.getWebpackConfig();
